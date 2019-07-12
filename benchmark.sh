1cad8b644295ff561895101f413a2c149f428249
DeLuca <delucaj96@gmail.com>
2019-07-12 14:03:20 -0700

    Benchmark script fixed, still used a cl option

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,4 @@
 script=./compile.sh
 
-find . -type f -name "*.mini" -exec echo $1 {} \; -exec $script $1 {} \;
+find . -type f -name "*.mini" -exec echo -stack {} \; -exec $script -stack {} \;
 find . -type f -name "*.mini" -exec echo {} \; -exec $script {} \;
