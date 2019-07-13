1179be6814c12743217f8ca64906a7520b220e7c
DeLuca <delucaj96@gmail.com>
2019-07-12 21:37:00 -0700

    Compiler Completed. Updating Benchmark script

--- benchmarkClang.sh
+++ benchmarkClang.sh
@@ -1,3 +1,10 @@
 script='clang -m32'
 
-find . -type f -name "*.ll" -exec echo {} \; -exec $script {} \;
+function compile {
+    file=$1
+    #echo ${file%%.ll}
+    $script -o ${file%%.ll} $1
+}
+
+export -f compile
+find . -type f -name "*.ll" -exec bash -c 'compile "$0"' {} \; 
